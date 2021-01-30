eafe78a66bc1a1de2172b945d34417f8090729f6
Breens-Thomas <mbakabreens@gmail.com>
2021-01-30 17:44:53 +0300

    Nested quote component to app component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,15 +1,5 @@
 export class Quote {
 
-    upvote:number;
-    downvote:number;
-
-    constructor(
-        public author:string,
-        public publisher:string,
-        public content:string,
-        public datePosted:Date
-        ) {
-        this.upvote = 0;
-        this.downvote = 0;
+    constructor(public author: string,public publisher: string,public content: string,public datePosted: Date) {
     }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  quotes:Quote[] = [
+    new Quote("Ryan","Breens","Pain is temporary but pride is forever",new Date()),
+    new Quote("Ryan","Breens","Pain is temporary but pride is forever",new Date()),
+    new Quote("Ryan","Breens","Pain is temporary but pride is forever",new Date())
+  ] 
 
   constructor() { }
 
