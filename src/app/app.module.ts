35bcb896ce107ae3a4dd57c90c42408e5cfd578f
Breens-Thomas <mbakabreens@gmail.com>
2021-02-01 13:04:30 +0300

    Blueprint class for creating quote objects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
-import { CreateQuoteComponent } from './create-quote/create-quote.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent,
-    QuoteDetailsComponent,
-    CreateQuoteComponent
+    
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,8 @@
 export class Quote {
+    constructor(
+    public author:string,
+    public publisher:string,
+    public content:string) {
 
-    constructor(public author: string,public publisher: string,public content: string,) {
     }
 }
